547d7c71bbd8b9370ff91c385e08e12722df34d7
黑发st <a2365127723@outlook.com>
2026-03-03 20:36:18 +0800

    更改config

--- Config/config.h
+++ Config/config.h
@@ -5,15 +5,15 @@
 #define USE_EEPROM 1 // 是否使用EEPROM
 
 // 网络配置
-#define WIFI_SSID            "*******"        // WiFi名称
-#define WIFI_PASSWORD        ""*******""           // WiFi密码
-#define MQTT_BROKER          ""*******""        // MQTT服务器地址
-#define MQTT_PORT            ""*******""                  // MQTT服务器端口
-#define MQTT_CLIENT_ID       ""*******""         // MQTT客户端ID
+#define WIFI_SSID            "小黑子怎么你了"        // WiFi名称
+#define WIFI_PASSWORD        "13640084506"           // WiFi密码
+#define MQTT_BROKER          "broker.emqx.io"        // MQTT服务器地址
+#define MQTT_PORT            "1883"                  // MQTT服务器端口
+#define MQTT_CLIENT_ID       "smartlock_772"         // MQTT客户端ID
 #define MQTT_USERNAME        ""                      // MQTT用户名
 #define MQTT_PASSWORD        ""                      // MQTT密码
-#define MQTT_TOPIC_SUBSCRIBE ""*******"" // MQTT订阅主题
-#define MQTT_TOPIC_PUBLISH   ""*******""  // MQTT发布主题
+#define MQTT_TOPIC_SUBSCRIBE "smartlock/772/control" // MQTT订阅主题
+#define MQTT_TOPIC_PUBLISH   "smartlock/772/status"  // MQTT发布主题
 
 // 门锁配置
 #define OPEN_ANGLE      73   // 开门角度
@@ -23,9 +23,9 @@
 #define ALARM_DURATION  60   // 报警持续时间(s)
 
 // 验证配置
-#define DEFAUT_PASSWORD ""*******""                               // 默认密码
-#define DEFAUT_CARDID_1 (uint8_t[4]){0x00, 0x00, 0x00, 0x00} // 默认卡号1
-#define DEFAUT_CARDID_2 (uint8_t[4]){0x00, 0x00, 0x00, 0x00} // 默认卡号2
-#define DEFAUT_CARDID_3 (uint8_t[4]){0x00, 0x00, 0x00, 0x00} // 默认卡号3
+#define DEFAUT_PASSWORD "0802"                               // 默认密码
+#define DEFAUT_CARDID_1 (uint8_t[4]){0x43, 0x03, 0x61, 0x27} // 默认卡号1
+#define DEFAUT_CARDID_2 (uint8_t[4]){0x53, 0xDE, 0xAF, 0x2C} // 默认卡号2
+#define DEFAUT_CARDID_3 (uint8_t[4]){0x30, 0x8D, 0x02, 0x89} // 默认卡号3
 
 #endif /* __CONFIG_H__ */
